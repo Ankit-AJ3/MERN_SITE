34891c3384d455b1aa36e3eadaec0fd8f05f6a3b
Ankit Jangid <ankitjangid8501@gmail.com>
2024-02-12 22:13:45 +0530

    6 commit

--- backend/middlewares/auth-middleware.js
+++ backend/middlewares/auth-middleware.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/user-model");
 
 const authMiddleware = async(req, res, next) => {
     const token = req.header("Authorization");
@@ -6,9 +7,27 @@ const authMiddleware = async(req, res, next) => {
     if(!token) {
         return res.status(401).json({ message: "Unauthorization HTTP, Token not provided"});
     }
-    console.log("token from auth middleware", token);
+    
+    const jwtToken = token.replace("Bearer", "").trim();
+    console.log("token from auth middleware", jwtToken);
 
-    next();
+    try {
+        const isVerified = jwt.verify(jwtToken, process.env.JWT_SECRET_KEY);
+
+        const userData = await User.findOne({ email: isVerified.email }).select({
+            password: 0,
+        });
+        console.log(userData);
+        
+        req.user = userData;
+        req.token = token;
+        req.userID = userData._id;
+
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: "Unahorized. Invalid token." });
+    }
+
 };
 
 module.exports = authMiddleware;
