d006391674a22e753f0f6406538e837e9fcccd1a
Ankit Jangid <ankitjangid8501@gmail.com>
2024-02-23 22:48:06 +0530

    22 commit

--- frontend/src/pages/Service.jsx
+++ frontend/src/pages/Service.jsx
@@ -1,43 +1,43 @@
-import { useAuth } from "../store/auth";
+// import { useAuth } from "../store/auth";
 
-const Service = () => {
-  const { services } = useAuth();
+// const Service = () => {
+//   const { services } = useAuth();
 
-  return ( 
+//   return ( 
     
-    <section className="section-services">
-      <div className="container">
-        <h1 className="main-heading">Services </h1>
-      </div>
+//     <section className="section-services">
+//       <div className="container">
+//         <h1 className="main-heading">Services </h1>
+//       </div>
 
-      <div className="container grid grid-three-cols">
-        {services.map((curElem, index) => {
-          const { price, description, provider, service } = curElem;
+//       <div className="container grid grid-three-cols">
+//         {services.map((curElem, index) => {
+//           const { price, description, provider, service } = curElem;
 
-          return (
-            <div className="card" key={index}>
-              <div className="card-img">
-                <img
-                  src="/images/design.png"
-                  alt="our services info"
-                  width="200"
-                />
-              </div>
+//           return (
+//             <div className="card" key={index}>
+//               <div className="card-img">
+//                 <img
+//                   src="/images/design.png"
+//                   alt="our services info"
+//                   width="200"
+//                 />
+//               </div>
 
-              <div className="card-details">
-                <div className="grid grid-two-cols">
-                  <p>{provider}</p>
-                  <p>{price}</p>
-                </div>
-                <h2>{service}</h2>
-                <p>{description}</p>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </section>
-  );
-};
+//               {/* <div className="card-details">
+//                 <div className="grid grid-two-cols">
+//                   <p>{provider}</p>
+//                   <p>{price}</p>
+//                 </div>
+//                 <h2>{service}</h2>
+//                 <p>{description}</p>
+//               </div>
+//             </div> */}
+//           );
+//         })}
+//       </div>
+//     </section>
+//   );
+// };
 
-export default Service;
+// export default Service;
